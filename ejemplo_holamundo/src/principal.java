723e1d74afe780057cacc5f984c07e1c6f973c04
LC1301319 <LC1301319>
2018-08-16 09:51:37 -0400

    primera subida

---
+++ ejemplo_holamundo/src/principal.java
@@ -0,0 +1,11 @@
+
+public class principal {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Ejemplo para github/gitlab");
+        
+    }
+    }
+    
